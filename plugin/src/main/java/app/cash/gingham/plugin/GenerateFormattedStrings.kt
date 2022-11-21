872cf5840339362aef0e6c558e69ce586ad0131d
Patrick Theisen <ptheisen@squareup.com>
2022-11-21 16:20:19 -0500

    Fix generator task

--- plugin/src/main/java/app/cash/gingham/plugin/GenerateFormattedStrings.kt
+++ plugin/src/main/java/app/cash/gingham/plugin/GenerateFormattedStrings.kt
@@ -6,15 +6,8 @@ import app.cash.gingham.plugin.model.TokenizedStringResource
 import app.cash.gingham.plugin.parser.parseStringResources
 import app.cash.icu.asIcuTokens
 import app.cash.icu.tokens.Argument
-import app.cash.icu.tokens.Date
+import app.cash.icu.tokens.ChoiceArgument
 import app.cash.icu.tokens.IcuToken
-import app.cash.icu.tokens.Literal
-import app.cash.icu.tokens.Number
-import app.cash.icu.tokens.Quantity
-import app.cash.icu.tokens.Quantity.Count
-import app.cash.icu.tokens.Select
-import app.cash.icu.tokens.Text
-import app.cash.icu.tokens.Time
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.DirectoryProperty
@@ -58,10 +51,9 @@ abstract class GenerateFormattedStrings @Inject constructor() : DefaultTask() {
 
   private fun IcuToken.findArguments(): Set<Argument> =
     when (this) {
-      is Count, is Literal -> emptySet()
-      is Date, is Number, is Text, is Time -> setOf(this as Argument)
-      is Quantity -> setOf(this) + choices.flatMap { it.value.findArguments() }
-      is Select -> setOf(this) + choices.flatMap { it.value.findArguments() }
+      !is Argument -> emptySet()
+      !is ChoiceArgument<*> -> setOf(this)
+      else -> setOf(this) + choices.flatMap { it.value.findArguments() }
     }
 
   private fun File.isStringResourceFile(): Boolean =
