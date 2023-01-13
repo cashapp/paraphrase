7b1eb1116a1581a467d2b79c3e4005c0187220b0
Patrick Theisen <ptheisen@squareup.com>
2023-01-13 13:34:01 -0800

    Use Gradle Worker in the GenerateFormattedResources task

--- plugin/src/main/java/com/squareup/cash/gingham/plugin/GenerateFormattedResources.kt
+++ plugin/src/main/java/com/squareup/cash/gingham/plugin/GenerateFormattedResources.kt
@@ -1,6 +1,8 @@
 // Copyright Square, Inc.
 package com.squareup.cash.gingham.plugin
 
+import java.io.File
+import javax.inject.Inject
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.DirectoryProperty
@@ -11,8 +13,10 @@ import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
-import java.io.File
-import javax.inject.Inject
+import org.gradle.workers.WorkAction
+import org.gradle.workers.WorkParameters
+import org.gradle.workers.WorkerExecutor
+
 
 /**
  * A Gradle task that reads all of the Android string resources in a module and then generates
@@ -34,18 +38,37 @@ internal abstract class GenerateFormattedResources @Inject constructor() : Defau
   @get:OutputDirectory
   abstract val outputDirectory: DirectoryProperty
 
+  @get:Inject
+  abstract val workerExecutor: WorkerExecutor
+
   @TaskAction
   fun generateFormattedStringResources() {
-    stringResourceFiles
-      .flatMap { parseResources(file = it) }
-      .map { tokenizeResource(rawResource = it) }
-      .filter { it.tokens.isNotEmpty() }
-      .let { writeResources(packageName = namespace.get(), tokenizedResources = it) }
-      .writeTo(directory = outputDirectory.get().asFile)
+    workerExecutor.noIsolation().submit(Action::class.java) { parameters ->
+      parameters.namespace.set(namespace)
+      parameters.stringResourceFiles.from(stringResourceFiles)
+      parameters.outputDirectory.set(outputDirectory)
+    }
   }
 
   private fun File.isStringResourceFile(): Boolean =
     isFile && bufferedReader().useLines { lines ->
       lines.any { it.trimStart().startsWith("<string ") }
     }
+
+  internal interface Parameters : WorkParameters {
+    val namespace: Property<String>
+    val stringResourceFiles: ConfigurableFileCollection
+    val outputDirectory: DirectoryProperty
+  }
+
+  internal abstract class Action : WorkAction<Parameters> {
+    override fun execute() {
+      parameters.stringResourceFiles
+        .flatMap { parseResources(file = it) }
+        .map { tokenizeResource(rawResource = it) }
+        .filter { it.tokens.isNotEmpty() }
+        .let { writeResources(packageName = parameters.namespace.get(), tokenizedResources = it) }
+        .writeTo(directory = parameters.outputDirectory.get().asFile)
+    }
+  }
 }
