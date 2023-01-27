1a466525ebcef8cc5c1f8b8022db96614930fdda
Jake Wharton <jw@squareup.com>
2023-01-27 16:47:30 -0500

    Fix the locale and timezone for testing
    Otherwise the tests fail if you're not set to English on the US east coast!

--- tests/src/main/kotlin/app/cash/gingham/tests/TypesTest.kt
+++ tests/src/main/kotlin/app/cash/gingham/tests/TypesTest.kt
@@ -5,12 +5,20 @@ import androidx.test.platform.app.InstrumentationRegistry
 import app.cash.gingham.getString
 import com.google.common.truth.Truth.assertThat
 import java.time.Instant
+import java.util.Locale
+import java.util.TimeZone
 import kotlin.time.Duration.Companion.hours
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
+import org.junit.Rule
 import org.junit.Test
 
 class TypesTest {
+  @get:Rule val localeRule = LocaleAndTimeZoneRule(
+    locale = Locale("en", "US"),
+    timeZone = TimeZone.getTimeZone("Pacific/Honolulu"),
+  )
+
   private val context = InstrumentationRegistry.getInstrumentation().context
   private val releaseDate = Instant.ofEpochSecond(1648185825)
 
@@ -54,62 +62,62 @@ class TypesTest {
 
   @Test fun typeDate() {
     val formatted = context.getString(FormattedResources.type_date(releaseDate))
-    assertThat(formatted).isEqualTo("A Mar 25, 2022 B")
+    assertThat(formatted).isEqualTo("A Mar 24, 2022 B")
   }
 
   @Test fun typeDateShort() {
     val formatted = context.getString(FormattedResources.type_date_short(releaseDate))
-    assertThat(formatted).isEqualTo("A 3/25/22 B")
+    assertThat(formatted).isEqualTo("A 3/24/22 B")
   }
 
   @Test fun typeDateMedium() {
     val formatted = context.getString(FormattedResources.type_date_medium(releaseDate))
-    assertThat(formatted).isEqualTo("A Mar 25, 2022 B")
+    assertThat(formatted).isEqualTo("A Mar 24, 2022 B")
   }
 
   @Test fun typeDateLong() {
     val formatted = context.getString(FormattedResources.type_date_long(releaseDate))
-    assertThat(formatted).isEqualTo("A March 25, 2022 B")
+    assertThat(formatted).isEqualTo("A March 24, 2022 B")
   }
 
   @Test fun typeDateFull() {
     val formatted = context.getString(FormattedResources.type_date_full(releaseDate))
-    assertThat(formatted).isEqualTo("A Friday, March 25, 2022 B")
+    assertThat(formatted).isEqualTo("A Thursday, March 24, 2022 B")
   }
 
   @Test fun typeDateCustom() {
     val formatted = context.getString(FormattedResources.type_date_custom(releaseDate))
-    assertThat(formatted).isEqualTo("A 2022-03-25 B")
+    assertThat(formatted).isEqualTo("A 2022-03-24 B")
   }
 
   @Test fun typeTime() {
     val formatted = context.getString(FormattedResources.type_time(releaseDate))
-    assertThat(formatted).isEqualTo("A 1:23:45 AM B")
+    assertThat(formatted).isEqualTo("A 7:23:45 PM B")
   }
 
   @Test fun typeTimeShort() {
     val formatted = context.getString(FormattedResources.type_time_short(releaseDate))
-    assertThat(formatted).isEqualTo("A 1:23 AM B")
+    assertThat(formatted).isEqualTo("A 7:23 PM B")
   }
 
   @Test fun typeTimeMedium() {
     val formatted = context.getString(FormattedResources.type_time_medium(releaseDate))
-    assertThat(formatted).isEqualTo("A 1:23:45 AM B")
+    assertThat(formatted).isEqualTo("A 7:23:45 PM B")
   }
 
   @Test fun typeTimeLong() {
     val formatted = context.getString(FormattedResources.type_time_long(releaseDate))
-    assertThat(formatted).isEqualTo("A 1:23:45 AM EDT B")
+    assertThat(formatted).isEqualTo("A 7:23:45 PM HST B")
   }
 
   @Test fun typeTimeFull() {
     val formatted = context.getString(FormattedResources.type_time_full(releaseDate))
-    assertThat(formatted).isEqualTo("A 1:23:45 AM Eastern Daylight Time B")
+    assertThat(formatted).isEqualTo("A 7:23:45 PM Hawaii-Aleutian Standard Time B")
   }
 
   @Test fun typeTimeCustom() {
     val formatted = context.getString(FormattedResources.type_time_custom(releaseDate))
-    assertThat(formatted).isEqualTo("A 01-23-45 B")
+    assertThat(formatted).isEqualTo("A 19-23-45 B")
   }
 
   @Test fun typeDuration() {
