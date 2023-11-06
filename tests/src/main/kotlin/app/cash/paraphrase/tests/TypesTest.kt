7c7bd7214e8415675d8dabef7b713e3e9cf1f1a5
Drew Hamilton <github@drewhamilton.dev>
2023-11-06 15:47:06 -0800

    Test ICU4J in TypesTest to compare results with Android ICU

--- tests/src/main/kotlin/app/cash/paraphrase/tests/TypesTest.kt
+++ tests/src/main/kotlin/app/cash/paraphrase/tests/TypesTest.kt
@@ -17,8 +17,13 @@ package app.cash.paraphrase.tests
 
 import android.os.Build
 import androidx.test.platform.app.InstrumentationRegistry
+import app.cash.paraphrase.AndroidDateTimeConverter
+import app.cash.paraphrase.FormattedResource
 import app.cash.paraphrase.getString
 import com.google.common.truth.Truth.assertThat
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import com.ibm.icu.text.MessageFormat as JvmMessageFormat
 import java.time.LocalDate
 import java.time.LocalTime
 import java.time.Month
@@ -30,10 +35,15 @@ import java.util.Locale
 import kotlin.time.Duration.Companion.hours
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
-class TypesTest {
+@RunWith(TestParameterInjector::class)
+class TypesTest(
+  @TestParameter private val icuImpl: IcuImpl,
+) {
   @get:Rule val localeRule = LocaleAndTimeZoneRule(
     locale = Locale("en", "US"),
   )
@@ -47,78 +57,85 @@ class TypesTest {
     ZoneId.of("Pacific/Honolulu"),
   )
 
+  @Before fun setDateTimeConverter() {
+    FormattedResources.dateTimeConverter = when (icuImpl) {
+      IcuImpl.Android -> AndroidDateTimeConverter
+      IcuImpl.Jvm -> JvmDateTimeConverter
+    }
+  }
+
   @Test fun typeNone() {
-    val formattedString = context.getString(FormattedResources.type_none("Z"))
+    val formattedString = getString(FormattedResources.type_none("Z"))
     assertThat(formattedString).isEqualTo("A Z B")
-    val formattedInteger = context.getString(FormattedResources.type_none(2))
+    val formattedInteger = getString(FormattedResources.type_none(2))
     assertThat(formattedInteger).isEqualTo("A 2 B")
-    val formattedDouble = context.getString(FormattedResources.type_none(2.345))
+    val formattedDouble = getString(FormattedResources.type_none(2.345))
     assertThat(formattedDouble).isEqualTo("A 2.345 B")
     val formattedInstant =
-      context.getString(FormattedResources.type_none(releaseDateTime.toInstant()))
+      getString(FormattedResources.type_none(releaseDateTime.toInstant()))
     assertThat(formattedInstant).isEqualTo("A 2022-03-25T05:23:45Z B")
   }
 
   @Test fun typeNumber() {
-    val formattedInteger = context.getString(FormattedResources.type_number(2))
+    val formattedInteger = getString(FormattedResources.type_number(2))
     assertThat(formattedInteger).isEqualTo("A 2 B")
-    val formattedDouble = context.getString(FormattedResources.type_number(2.345))
+    val formattedDouble = getString(FormattedResources.type_number(2.345))
     assertThat(formattedDouble).isEqualTo("A 2.345 B")
   }
 
   @Test fun typeNumberInteger() {
-    val formatted = context.getString(FormattedResources.type_number_integer(2))
+    val formatted = getString(FormattedResources.type_number_integer(2))
     assertThat(formatted).isEqualTo("A 2 B")
   }
 
   @Test fun typeNumberCurrency() {
-    val formatted = context.getString(FormattedResources.type_number_currency(2))
+    val formatted = getString(FormattedResources.type_number_currency(2))
     assertThat(formatted).isEqualTo("A $2.00 B")
   }
 
   @Test fun typeNumberPercent() {
-    val formatted = context.getString(FormattedResources.type_number_percent(.2))
+    val formatted = getString(FormattedResources.type_number_percent(.2))
     assertThat(formatted).isEqualTo("A 20% B")
   }
 
   @Test fun typeNumberCustom() {
-    val formatted = context.getString(FormattedResources.type_number_custom(1234567))
+    val formatted = getString(FormattedResources.type_number_custom(1234567))
     assertThat(formatted).isEqualTo("A 12,34,567 B")
   }
 
   @Test fun typeDate() {
-    val formatted = context.getString(FormattedResources.type_date(releaseDate))
+    val formatted = getString(FormattedResources.type_date(releaseDate))
     assertThat(formatted).isEqualTo("A Mar 24, 2022 B")
   }
 
   @Test fun typeDateShort() {
-    val formatted = context.getString(FormattedResources.type_date_short(releaseDate))
+    val formatted = getString(FormattedResources.type_date_short(releaseDate))
     assertThat(formatted).isEqualTo("A 3/24/22 B")
   }
 
   @Test fun typeDateMedium() {
-    val formatted = context.getString(FormattedResources.type_date_medium(releaseDate))
+    val formatted = getString(FormattedResources.type_date_medium(releaseDate))
     assertThat(formatted).isEqualTo("A Mar 24, 2022 B")
   }
 
   @Test fun typeDateLong() {
-    val formatted = context.getString(FormattedResources.type_date_long(releaseDate))
+    val formatted = getString(FormattedResources.type_date_long(releaseDate))
     assertThat(formatted).isEqualTo("A March 24, 2022 B")
   }
 
   @Test fun typeDateFull() {
-    val formatted = context.getString(FormattedResources.type_date_full(releaseDate))
+    val formatted = getString(FormattedResources.type_date_full(releaseDate))
     assertThat(formatted).isEqualTo("A Thursday, March 24, 2022 B")
   }
 
   @Test fun typeDatePatternDateTimeZone() {
     val formatted =
-      context.getString(FormattedResources.type_date_pattern_date_time_zone(releaseDateTime))
+      getString(FormattedResources.type_date_pattern_date_time_zone(releaseDateTime))
     assertThat(formatted).isEqualTo("A 3-24, 7PM HST B")
   }
 
   @Test fun typeDatePatternDateTimeOffset() {
-    val formatted = context.getString(
+    val formatted = getString(
       FormattedResources.type_date_pattern_date_time_offset(releaseDateTime.toOffsetDateTime()),
     )
     assertThat(formatted).isEqualTo("A 3-24, 7PM -10:00 B")
@@ -126,36 +143,36 @@ class TypesTest {
 
   @Test fun typeDatePatternDateTime() {
     val localDateTime = releaseDateTime.toLocalDateTime()
-    val formatted = context.getString(FormattedResources.type_date_pattern_date_time(localDateTime))
+    val formatted = getString(FormattedResources.type_date_pattern_date_time(localDateTime))
     assertThat(formatted).isEqualTo("A 3-24 7PM B")
   }
 
   @Test fun typeDatePatternDateZone() {
     val formatted =
-      context.getString(FormattedResources.type_date_pattern_date_zone(releaseDateTime))
+      getString(FormattedResources.type_date_pattern_date_zone(releaseDateTime))
     assertThat(formatted).isEqualTo("A March (HST) B")
   }
 
   @Test fun typeDatePatternDateOffset() {
-    val formatted = context.getString(
+    val formatted = getString(
       FormattedResources.type_date_pattern_date_offset(releaseDateTime.toOffsetDateTime()),
     )
     assertThat(formatted).isEqualTo("A March (-10:00) B")
   }
 
   @Test fun typeDatePatternDate() {
-    val formatted = context.getString(FormattedResources.type_date_pattern_date(releaseDate))
+    val formatted = getString(FormattedResources.type_date_pattern_date(releaseDate))
     assertThat(formatted).isEqualTo("A 2022-03-24 B")
   }
 
   @Test fun typeDatePatternTimeZone() {
     val formatted =
-      context.getString(FormattedResources.type_date_pattern_time_zone(releaseDateTime))
+      getString(FormattedResources.type_date_pattern_time_zone(releaseDateTime))
     assertThat(formatted).isEqualTo("A 19:23 HST B")
   }
 
   @Test fun typeDatePatternTimeOffset() {
-    val formatted = context.getString(
+    val formatted = getString(
       FormattedResources.type_date_pattern_time_offset(
         // Ensures the UTC/GMT case works:
         releaseDateTime.withZoneSameLocal(ZoneOffset.UTC).toOffsetDateTime().toOffsetTime(),
@@ -165,60 +182,60 @@ class TypesTest {
   }
 
   @Test fun typeDatePatternTime() {
-    val formatted = context.getString(FormattedResources.type_date_pattern_time(releaseTime))
+    val formatted = getString(FormattedResources.type_date_pattern_time(releaseTime))
     assertThat(formatted).isEqualTo("A 23 past 7 B")
   }
 
   @Test fun typeDatePatternZone() {
-    val formatted = context.getString(FormattedResources.type_date_pattern_zone(releaseDateTime))
+    val formatted = getString(FormattedResources.type_date_pattern_zone(releaseDateTime))
     assertThat(formatted).isEqualTo("A Hawaii-Aleutian Standard Time B")
   }
 
   @Test fun typeDatePatternOffset() {
-    val formatted = context.getString(
+    val formatted = getString(
       FormattedResources.type_date_pattern_offset(releaseDateTime.offset),
     )
     assertThat(formatted).isEqualTo("A GMT-10:00 B")
   }
 
   @Test fun typeDatePatternNone() {
-    val formatted = context.getString(FormattedResources.type_date_pattern_none(null))
+    val formatted = getString(FormattedResources.type_date_pattern_none(null))
     assertThat(formatted).isEqualTo("A What is this for? B")
   }
 
   @Test fun typeTime() {
-    val formatted = context.getString(FormattedResources.type_time(releaseTime))
+    val formatted = getString(FormattedResources.type_time(releaseTime))
     assertThat(formatted).isEqualTo("A 7:23:45 PM B")
   }
 
   @Test fun typeTimeShort() {
-    val formatted = context.getString(FormattedResources.type_time_short(releaseTime))
+    val formatted = getString(FormattedResources.type_time_short(releaseTime))
     assertThat(formatted).isEqualTo("A 7:23 PM B")
   }
 
   @Test fun typeTimeMedium() {
-    val formatted = context.getString(FormattedResources.type_time_medium(releaseTime))
+    val formatted = getString(FormattedResources.type_time_medium(releaseTime))
     assertThat(formatted).isEqualTo("A 7:23:45 PM B")
   }
 
   @Test fun typeTimeLong() {
-    val formatted = context.getString(FormattedResources.type_time_long(releaseDateTime))
+    val formatted = getString(FormattedResources.type_time_long(releaseDateTime))
     assertThat(formatted).isEqualTo("A 7:23:45 PM HST B")
   }
 
   @Test fun typeTimeFull() {
-    val formatted = context.getString(FormattedResources.type_time_full(releaseDateTime))
+    val formatted = getString(FormattedResources.type_time_full(releaseDateTime))
     assertThat(formatted).isEqualTo("A 7:23:45 PM Hawaii-Aleutian Standard Time B")
   }
 
   @Test fun typeTimePatternDateTimeZone() {
     val formatted =
-      context.getString(FormattedResources.type_time_pattern_date_time_zone(releaseDateTime))
+      getString(FormattedResources.type_time_pattern_date_time_zone(releaseDateTime))
     assertThat(formatted).isEqualTo("A 3-24, 7PM HST B")
   }
 
   @Test fun typeTimePatternDateTimeOffset() {
-    val formatted = context.getString(
+    val formatted = getString(
       FormattedResources.type_time_pattern_date_time_offset(releaseDateTime.toOffsetDateTime()),
     )
     assertThat(formatted).isEqualTo("A 3-24, 7PM -10 B")
@@ -226,36 +243,36 @@ class TypesTest {
 
   @Test fun typeTimePatternDateTime() {
     val localDateTime = releaseDateTime.toLocalDateTime()
-    val formatted = context.getString(FormattedResources.type_time_pattern_date_time(localDateTime))
+    val formatted = getString(FormattedResources.type_time_pattern_date_time(localDateTime))
     assertThat(formatted).isEqualTo("A 3-24 7PM B")
   }
 
   @Test fun typeTimePatternDateZone() {
     val formatted =
-      context.getString(FormattedResources.type_time_pattern_date_zone(releaseDateTime))
+      getString(FormattedResources.type_time_pattern_date_zone(releaseDateTime))
     assertThat(formatted).isEqualTo("A March (HST) B")
   }
 
   @Test fun typeTimePatternDateOffset() {
-    val formatted = context.getString(
+    val formatted = getString(
       FormattedResources.type_time_pattern_date_offset(releaseDateTime.toOffsetDateTime()),
     )
     assertThat(formatted).isEqualTo("A March (-10) B")
   }
 
   @Test fun typeTimePatternDate() {
-    val formatted = context.getString(FormattedResources.type_time_pattern_date(releaseDate))
+    val formatted = getString(FormattedResources.type_time_pattern_date(releaseDate))
     assertThat(formatted).isEqualTo("A 2022-03-24 B")
   }
 
   @Test fun typeTimePatternTimeZone() {
     val formatted =
-      context.getString(FormattedResources.type_time_pattern_time_zone(releaseDateTime))
+      getString(FormattedResources.type_time_pattern_time_zone(releaseDateTime))
     assertThat(formatted).isEqualTo("A 19:23 HST B")
   }
 
   @Test fun typeTimePatternTimeOffset() {
-    val formatted = context.getString(
+    val formatted = getString(
       FormattedResources.type_time_pattern_time_offset(
         OffsetTime.of(releaseDateTime.toLocalTime(), releaseDateTime.offset),
       ),
@@ -264,24 +281,24 @@ class TypesTest {
   }
 
   @Test fun typeTimePatternTime() {
-    val formatted = context.getString(FormattedResources.type_time_pattern_time(releaseTime))
+    val formatted = getString(FormattedResources.type_time_pattern_time(releaseTime))
     assertThat(formatted).isEqualTo("A 19-23-45 B")
   }
 
   @Test fun typeTimePatternZone() {
-    val formatted = context.getString(FormattedResources.type_time_pattern_zone(releaseDateTime))
+    val formatted = getString(FormattedResources.type_time_pattern_zone(releaseDateTime))
     assertThat(formatted).isEqualTo("A Hawaii-Aleutian Standard Time B")
   }
 
   @Test fun typeTimePatternOffset() {
-    val formatted = context.getString(
+    val formatted = getString(
       FormattedResources.type_time_pattern_offset(releaseDateTime.offset),
     )
     assertThat(formatted).isEqualTo("A GMT-10:00 B")
   }
 
   @Test fun typeTimePatternNone() {
-    val formatted = context.getString(FormattedResources.type_time_pattern_none(null))
+    val formatted = getString(FormattedResources.type_time_pattern_none(null))
     assertThat(formatted).isEqualTo("A What is this for? B")
   }
 
@@ -291,7 +308,7 @@ class TypesTest {
       LocalTime.NOON,
       ZoneId.of("America/Chicago"),
     )
-    val formatted = context.getString(FormattedResources.type_time_long(winterDateTime))
+    val formatted = getString(FormattedResources.type_time_long(winterDateTime))
     assertThat(formatted).isEqualTo("A 12:00:00 PM CST B")
   }
 
@@ -301,33 +318,33 @@ class TypesTest {
       LocalTime.NOON,
       ZoneId.of("America/Chicago"),
     )
-    val formatted = context.getString(FormattedResources.type_time_long(summerDateTime))
+    val formatted = getString(FormattedResources.type_time_long(summerDateTime))
     assertThat(formatted).isEqualTo("A 12:00:00 PM CDT B")
   }
 
   @Test fun typeDuration() {
-    val formattedSeconds = context.getString(FormattedResources.type_duration(3.seconds))
+    val formattedSeconds = getString(FormattedResources.type_duration(3.seconds))
     assertThat(formattedSeconds).isEqualTo("A 3 sec. B")
-    val formattedMinutes = context.getString(FormattedResources.type_duration(3.minutes + 2.seconds))
+    val formattedMinutes = getString(FormattedResources.type_duration(3.minutes + 2.seconds))
     assertThat(formattedMinutes).isEqualTo("A 3:02 B")
-    val formattedHours = context.getString(FormattedResources.type_duration(3.hours + 2.minutes + 1.seconds))
+    val formattedHours = getString(FormattedResources.type_duration(3.hours + 2.minutes + 1.seconds))
     assertThat(formattedHours).isEqualTo("A 3:02:01 B")
   }
 
   @Test fun typeOrdinal() {
     val zero = 0 // Requires an int overload to be invoked
-    val formattedZero = context.getString(FormattedResources.type_ordinal(zero))
+    val formattedZero = getString(FormattedResources.type_ordinal(zero))
     assertThat(formattedZero).isEqualTo("A 0th B")
-    val formattedOne = context.getString(FormattedResources.type_ordinal(1))
+    val formattedOne = getString(FormattedResources.type_ordinal(1))
     assertThat(formattedOne).isEqualTo("A 1st B")
-    val formattedTwo = context.getString(FormattedResources.type_ordinal(2))
+    val formattedTwo = getString(FormattedResources.type_ordinal(2))
     assertThat(formattedTwo).isEqualTo("A 2nd B")
-    val formattedThree = context.getString(FormattedResources.type_ordinal(3))
+    val formattedThree = getString(FormattedResources.type_ordinal(3))
     assertThat(formattedThree).isEqualTo("A 3rd B")
-    val formattedFour = context.getString(FormattedResources.type_ordinal(4))
+    val formattedFour = getString(FormattedResources.type_ordinal(4))
     assertThat(formattedFour).isEqualTo("A 4th B")
-    val formattedLong = context.getString(FormattedResources.type_ordinal(Long.MAX_VALUE))
-    val expected = if (Build.VERSION.SDK_INT >= 26) {
+    val formattedLong = getString(FormattedResources.type_ordinal(Long.MAX_VALUE))
+    val expected = if (Build.VERSION.SDK_INT >= 26 || icuImpl == IcuImpl.Jvm) {
       "9,223,372,036,854,775,807th"
     } else {
       // ICU versions on older Android platforms lose bits by internally converting Long to Double:
@@ -337,29 +354,44 @@ class TypesTest {
   }
 
   @Test fun typeSpellout() {
-    val formattedOnes = context.getString(FormattedResources.type_spellout(3))
+    val formattedOnes = getString(FormattedResources.type_spellout(3))
     assertThat(formattedOnes).isEqualTo("A three B")
-    val formattedTens = context.getString(FormattedResources.type_spellout(32))
+    val formattedTens = getString(FormattedResources.type_spellout(32))
     assertThat(formattedTens).isEqualTo("A thirty-two B")
-    val formattedHundreds = context.getString(FormattedResources.type_spellout(321))
+    val formattedHundreds = getString(FormattedResources.type_spellout(321))
     assertThat(formattedHundreds).isEqualTo("A three hundred twenty-one B")
   }
 
   @Test fun typePlural() {
-    val formatted0 = context.getString(FormattedResources.type_count_plural(0))
+    val formatted0 = getString(FormattedResources.type_count_plural(0))
     assertThat(formatted0).isEqualTo("A Z B")
-    val formatted1 = context.getString(FormattedResources.type_count_plural(1))
+    val formatted1 = getString(FormattedResources.type_count_plural(1))
     assertThat(formatted1).isEqualTo("A Y B")
-    val formatted2 = context.getString(FormattedResources.type_count_plural(2))
+    val formatted2 = getString(FormattedResources.type_count_plural(2))
     assertThat(formatted2).isEqualTo("A X B")
   }
 
   @Test fun typeSelect() {
-    val formattedAlpha = context.getString(FormattedResources.type_verse_select("alpha"))
+    val formattedAlpha = getString(FormattedResources.type_verse_select("alpha"))
     assertThat(formattedAlpha).isEqualTo("A Z B")
-    val formattedBeta = context.getString(FormattedResources.type_verse_select("beta"))
+    val formattedBeta = getString(FormattedResources.type_verse_select("beta"))
     assertThat(formattedBeta).isEqualTo("A Y B")
-    val formattedGamma = context.getString(FormattedResources.type_verse_select("gamma"))
+    val formattedGamma = getString(FormattedResources.type_verse_select("gamma"))
     assertThat(formattedGamma).isEqualTo("A X B")
+  }
+
+  private fun getString(formattedResource: FormattedResource): String {
+    return when (icuImpl) {
+      IcuImpl.Android -> context.getString(formattedResource)
+      IcuImpl.Jvm -> JvmMessageFormat(context.getString(formattedResource.id))
+        .format(formattedResource.arguments)
+        // Android doesn't use ' ', so replace with a normal space for consistency:
+        .replace(' ', ' ')
+    }
+  }
+
+  enum class IcuImpl {
+    Android,
+    Jvm,
   }
 }
